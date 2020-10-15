4b2d6af83bd1f3e538f45f33ec4ee62d719d80c7
Olivia Toolson <oliviatoolson@Olivias-MacBook-Pro.local>
2020-10-15 12:36:18 -0600

    created skeletons for test

---
+++ cypress/integration/examples/form_test.js
@@ -0,0 +1,32 @@
+// write tests here
+describe('User Onboarding Test', () => {
+
+    it('Get name input and type a name', () => {
+
+    })
+
+    it('Check if text input contains the name provided', () => {
+
+    })
+
+    it('Get email input and type an email address', () => {
+
+    })
+
+    it('Get password input and type a password', () => {
+
+    })
+
+    it('Check to see if user can check TOS checkbox', () => {
+
+    })
+
+    it('Check if a user can submit the form data', () => {
+
+    })
+
+    it('Check for form validation if an input is left empty', () => {
+        
+    })
+
+})
